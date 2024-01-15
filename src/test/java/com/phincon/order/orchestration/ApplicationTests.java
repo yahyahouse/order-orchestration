841c9976f17acd19d6e528f8947302fe9b03898b
yahya wijaya <yahyawijaya082@gmail.com>
2024-01-15 17:37:30 +0700

    test : add test case for application

--- src/test/java/com/phincon/order/orchestration/ApplicationTests.java
+++ src/test/java/com/phincon/order/orchestration/ApplicationTests.java
@@ -3,11 +3,14 @@ package com.phincon.order.orchestration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThrows(IllegalArgumentException.class, () -> Application.main(null));
 	}
 
 }
