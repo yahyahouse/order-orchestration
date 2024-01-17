ff84e5f2f834f26eb77f4c70df25d7f4af2165c5
yahya wijaya <yahyawijaya082@gmail.com>
2024-01-17 09:28:22 +0700

    feat : add new data to constructor

--- src/test/java/com/phincon/order/orchestration/model/StepsTest.java
+++ src/test/java/com/phincon/order/orchestration/model/StepsTest.java
@@ -10,7 +10,7 @@ class StepsTest {
 
     @Test
     void testStepsConstructor() {
-        Steps steps = new Steps("1", "1", "step", 1);
+        Steps steps = new Steps("1", "1", "step", "s",1 );
         assertEquals("1", steps.getId());
         assertEquals("1", steps.getActionId());
         assertEquals("step", steps.getSteps());
